65b361e89cb81a358273d0fb90ddf47aa1d9d695
yasin-uzun <yasin.delco@gmail.com>
2020-07-29 09:29:36 -0400

    Alpha

--- Test.R
+++ Test.R
@@ -1,6 +1,17 @@
-library(MethylPipe)
+library(SINBAD)
+SINBAD::test()
 
 config_dir = '/mnt/isilon/tan_lab/uzuny/projects/jamboree/config_files/'
 read_configs(config_dir)
 
-MethylPipe::test()
+example_dir = '/mnt/isilon/tan_lab/uzuny/projects/jamboree/data/example/'
+raw_fastq_dir = paste0(example_dir, '/reads/')
+demux_index_file = paste0(example_dir, '/demux_index/')
+working_dir = paste0(example_dir, '/working_dir/')
+
+process_sample_wrapper(raw_fastq_dir = raw_fastq_dir,
+                       demux_index_file = demux_index_file,
+                       working_dir = working_dir,
+                       sample_name = 'Test')
+
+
